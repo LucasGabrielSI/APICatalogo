49eda54aa83fe4b33d4211b84dbaf9d320960a64
Lucas Gabriel de Oliveira Bento <lucasiguape16@gmail.com>
2022-10-06 12:04:48 -0300

    Add project files.

---
+++ APICatalogo/Migrations/20221005210356_PopulaCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into categorias(Nome, ImagemUrl) values('Bebida', 'bebidas.jpg')");
+            migrationBuilder.Sql("insert into categorias(Nome, ImagemUrl) values('Lanches', 'lanches.jpg')");
+            migrationBuilder.Sql("insert into categorias(Nome, ImagemUrl) values('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from categorias");
+        }
+    }
+}
---
+++ APICatalogo/Migrations/20221005211029_PopulaProdutos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations {
+    public partial class PopulaProdutos : Migration {
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.Sql("insert into produtos(" +
+              "Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)" +
+              "values('Coca-cola', 'Refrigerante de cola 350ml', '5.45', 'cocacola.jpg', '50', now(), 1)");
+            migrationBuilder.Sql("insert into produtos(" +
+              "Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)" +
+              "values('Misto quente', 'Pão com queijo e presunto assado na chapa.', '4.00', 'mistoquente.jpg', '13', now(), 2)");
+            migrationBuilder.Sql("insert into produtos(" +
+              "Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId)" +
+              "values('Sundae', 'Copo com 300ml, sabor balniha com calda de chocolate.', '3.50', 'sundae.jpg', '05', now(), 3)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            migrationBuilder.Sql("delete from produtos");
+        }
+    }
+}
